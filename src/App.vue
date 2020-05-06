f40629438cbc75f30de60ae0aecbc18a2449459d
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2020-05-06 15:37:49 +0700

    Add fotter

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
     <Header />
+    <p/>
+    <Footer />
   </div>
 </template>
+
 <script>
-import Header from './components/header/index.vue';
+import Header from './components/header/Header.vue';
+import Footer from './components/footer/Footer.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
